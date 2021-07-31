b537285ae706451ebb3b1b860ecfe1390eacac4b
williammlekush <56037586+williammlekush@users.noreply.github.com>
2021-07-31 11:28:48 -1000

    chore: format files

--- 04_JSWay_Exercises/201022-bank-exercise.js
+++ 04_JSWay_Exercises/201022-bank-exercise.js
@@ -1,37 +1,36 @@
 // This object describes a bank account
 function account(name = "Alex", startingBalance = 0) {
-   
-   return{
-      // Properties for name and balance
-      name: name,
-      balance: startingBalance,
+  return {
+    // Properties for name and balance
+    name: name,
+    balance: startingBalance,
 
-      // This method adds a given value to the balance
-      credit: function(val)  {
-         this.balance += val;
-      },
-      
-      // This method returns the owner and balance on the account
-      describe: function() {
-         console.log("owner: " + this.name, "balance " + this.balance);
-      }
-   }
+    // This method adds a given value to the balance
+    credit: function (val) {
+      this.balance += val;
+    },
+
+    // This method returns the owner and balance on the account
+    describe: function () {
+      console.log("owner: " + this.name, "balance " + this.balance);
+    },
+  };
 }
 
 const bankAccounts = [];
-const prospectiveCustomers = ["John", "Paul", "George", "Ringo"]
+const prospectiveCustomers = ["John", "Paul", "George", "Ringo"];
 
 // This creates an account for each customer and push it into the accounts array
-prospectiveCustomers.forEach(customer => {
-   bankAccounts.push(account(customer, 0));
-})
+prospectiveCustomers.forEach((customer) => {
+  bankAccounts.push(account(customer, 0));
+});
 
 // This adds ten to each account balance
-bankAccounts.forEach(account => {
-   account.describe();
-   account.credit(10);
-   account.describe();
-})
+bankAccounts.forEach((account) => {
+  account.describe();
+  account.credit(10);
+  account.describe();
+});
 
 const accountAlex = account();
 
--- 05_DOM/main.js
+++ 05_DOM/main.js
@@ -1,64 +1,62 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
 
-const changeEveryElement = (elementsArr = [], newText = '') => {
-   elementsArr.forEach((elem) => {
-      elem.innerHTML = newText;
-   });
-}
+const changeEveryElement = (elementsArr = [], newText = "") => {
+  elementsArr.forEach((elem) => {
+    elem.innerHTML = newText;
+  });
+};
 
-const changeSlogan = ({group, sloganText}) => {
-   const sloganDiv = document.getElementById(group + '-slogan');
-   
-   const newSlogan = document.createElement('div');
-   newSlogan.id = group + '-slogan-text';
-   newSlogan.textContent = sloganText;
-   sloganDiv.appendChild(newSlogan);
-}
+const changeSlogan = ({ group, sloganText }) => {
+  const sloganDiv = document.getElementById(group + "-slogan");
+
+  const newSlogan = document.createElement("div");
+  newSlogan.id = group + "-slogan-text";
+  newSlogan.textContent = sloganText;
+  sloganDiv.appendChild(newSlogan);
+};
 
 const countParagraphs = () => {
+  const paragraphArray = Array.from(document.getElementsByTagName("p"));
+  changeEveryElement(paragraphArray, "We are paragraphs.");
 
-   const paragraphArray = Array.from(document.getElementsByTagName('p'));
-   changeEveryElement(paragraphArray, 'We are paragraphs.')
+  const paragraphsSloganReplacer = {
+    group: "paragraphs",
+    sloganText: `PARAGRAPH POWER LEVELS X ${paragraphArray.length} !!!!!`,
+  };
 
-   const paragraphsSloganReplacer = {
-      group: 'paragraphs',
-      sloganText: `PARAGRAPH POWER LEVELS X ${paragraphArray.length} !!!!!`
-   };
-
-   changeSlogan(paragraphsSloganReplacer);
-
-}
+  changeSlogan(paragraphsSloganReplacer);
+};
 
 const countQuotes = () => {
+  const quotesArray = Array.from(
+    document.getElementById("quotes").getElementsByTagName("p")
+  );
+  changeEveryElement(quotesArray, "We are quotes.");
 
-const quotesArray = Array.from(document.getElementById('quotes').getElementsByTagName('p'));
-changeEveryElement(quotesArray, "We are quotes.");
+  const quotesSloganReplacer = {
+    group: "quotes",
+    sloganText: `We ${quotesArray.length} quotes quash any quary!`,
+  };
 
-
-const quotesSloganReplacer = {
-   group: 'quotes',
-   sloganText: `We ${quotesArray.length} quotes quash any quary!`
+  changeSlogan(quotesSloganReplacer);
 };
-
-changeSlogan(quotesSloganReplacer);
-}
 
 const countPoems = () => {
+  const poemsArray = Array.from(
+    document.getElementById("poems").getElementsByTagName("p")
+  );
+  changeEveryElement(poemsArray, "We are poems.");
 
-const poemsArray = Array.from(document.getElementById('poems').getElementsByTagName('p'));
-changeEveryElement(poemsArray, "We are poems.");
+  const poemsSloganReplacer = {
+    group: "poems",
+    sloganText: `We ${poemsArray.length} poems pummel any who challenge our position.`,
+  };
 
-const poemsSloganReplacer = {
-   group: 'poems',
-   sloganText: `We ${poemsArray.length} poems pummel any who challenge our position.`
+  changeSlogan(poemsSloganReplacer);
 };
 
-changeSlogan(poemsSloganReplacer);
-
-}
-
-button1.addEventListener('click', countParagraphs);
-button2.addEventListener('click', countQuotes);
-button3.addEventListener('click', countPoems);
+button1.addEventListener("click", countParagraphs);
+button2.addEventListener("click", countQuotes);
+button3.addEventListener("click", countPoems);
